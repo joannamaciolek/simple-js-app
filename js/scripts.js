07e06cd27c3a1c801bab7e307a939ca3dc6f5527
joanna_m85 <joanna.maciolek@gmail.com>
2019-02-18 23:18:55 +0100

    added the code to create array,loop & condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-alert('Hello world');
+var repository = [
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    types :['blaze' , 'solar-power']
+  } ,
+  {
+    name: 'Blastoise',
+    height: 1.6 ,
+    types :['water' , 'torrent']
+  } ,
+  {
+    name: 'Pidgeotto',
+    height: 1 ,
+    types :['flying' , 'keen-eye']
+  } ,
+  {
+    name: 'Sandshrew',
+    height: 0.6 ,
+    types :['sand' , 'earth']
+  }
+];
+
+for(var i = 0, l = repository.length; i < l; i++) {
+  if(repository[i].height > 1.1) {
+    document.write( '<li>' + repository[i].name + ' (height:' + repository[i].height + ') Wow that is BIG !');
+  } else {
+document.write( '<li>' + repository[i].name + ' (height:' + repository[i].height + ')');
+}
+}
