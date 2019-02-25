78af25accf8435fa8895bed43257f8ac3b509918
joanna_m85 <joanna.maciolek@gmail.com>
2019-02-25 20:52:17 +0100

    added comments to explain what specific functions do

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,16 @@ var pokemonRepository = (function () {
     types :['sand' , 'earth']
   }
   ];
-
+// adds item to the repository
   function add(item) {
+    //validates if item is object
     if (typeof item === 'object' ){
       repository.push(item);
     }else{
       console.log('new item is not an object therefore it cannot be added');
     }
   }
-
+// allows to access the repository from outside
   function getAll() {
     return repository;
 }
@@ -40,8 +41,9 @@ var pokemonRepository = (function () {
 };
 
 } ) ();
-
+// iterates over each object in repository and prints its name & height to the website
 pokemonRepository.getAll().forEach(function (eachName){
+  //checks if height is above 1.1 and adds extra comment
     if(eachName.height > 1.1) {
       document.write( '<li>' + eachName.name + ' (height:' + eachName.height + ') Wow that is BIG !');
      }
