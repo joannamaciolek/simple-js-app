e60e042c31e1ba336ff7718a701f80c213f3a512
joanna_m85 <joanna.maciolek@gmail.com>
2019-03-03 16:09:10 +0100

    added function to create DOM nodes from Pokemon repository, added event listener

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,49 @@ var pokemonRepository = (function () {
     name: 'Sandshrew',
     height: 0.6 ,
     types :['sand' , 'earth']
+  } ,
+  {
+    name: 'Snorlax',
+    height: 0.9 ,
+    types :['thick-fat' , 'immunity']
   }
   ];
-// adds item to the repository
+
+  //Creates new elements and adds into the DOM
+  function addListItem (eachPokemon) {
+    //creates li element in DOM
+    var $li = document.createElement('li');
+
+    // chooses ul element
+    var $ul = document.querySelector('ul');
+
+    //appends li DOM node as child of ul
+    $ul.appendChild($li);
+
+    //creates button element in DOM
+    var $button = document.createElement('button');
+
+    //adds class to button
+    $button.classList.add('button-style');
+
+    //appends button to website as a child of li
+    $li.appendChild($button);
+
+    // prints Pokemon name from repository to a button
+    $button.innerText = eachPokemon.name;
+
+    //executes showDetails function if button is clicked
+    $button.addEventListener('click',function(event){
+      console.log(eachPokemon.name);
+    })
+  }
+
+   //prints pokemon name to console when button is clicked
+  function showDetails(eachPokemon){
+    console.log(eachPokemon.name);
+    }
+
+  // adds item to the repository
   function add(item) {
     //validates if item is object
     if (typeof item === 'object' ){
@@ -30,24 +70,22 @@ var pokemonRepository = (function () {
       console.log('new item is not an object therefore it cannot be added');
     }
   }
-// allows to access the repository from outside
+
+  // allows to access the repository from outside
   function getAll() {
     return repository;
-}
+  }
 
   return {
+    addListItem: addListItem,
+    showDetails: showDetails,
     add: add,
     getAll: getAll
-};
+  };
 
 } ) ();
-// iterates over each object in repository and prints its name & height to the website
-pokemonRepository.getAll().forEach(function (eachName){
-  //checks if height is above 1.1 and adds extra comment
-    if(eachName.height > 1.1) {
-      document.write( '<li>' + eachName.name + ' (height:' + eachName.height + ') Wow that is BIG !');
-     }
-    else {
-      document.write( '<li>' + eachName.name + ' (height:' + eachName.height + ')');
-    }
+
+//calls addListItem function for each object in Pokemon repository
+pokemonRepository.getAll().forEach(function (eachPokemon){
+   pokemonRepository.addListItem (eachPokemon);
 });
